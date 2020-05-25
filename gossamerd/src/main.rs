e05b0bed285e73f66c1f72464b51e58fc37d4f28
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-25 11:09:06 -0700

    wip: daemon initial setup

--- gossamerd/src/main.rs
+++ gossamerd/src/main.rs
@@ -1,25 +1,37 @@
 use async_trait::async_trait;
+use libgossamer::engine::state::ServerId;
 use libgossamer::engine::{Engine, EngineHandle, Handler, Network};
 use tokio::prelude::*;
 
-// struct MyHandler {}
+struct IrcdEvent {}
 
-// #[async_trait]
-// impl Handler for MyHandler {
-//     async fn on_startup(&mut self, network: &mut Network) {
-//         println!("startup!");
-//     }
-// }
+struct Ircd {}
+
+#[async_trait]
+impl Handler<IrcdEvent> for Ircd {
+    async fn on_startup(&mut self, network: &mut Network) {
+        println!("startup!");
+    }
+
+    async fn on_event(&mut self, network: &mut Network, event: IrcdEvent) {
+        unimplemented!()
+    }
+
+    async fn on_link(&mut self, network: &mut Network, peer: ServerId) {
+        unimplemented!()
+    }
+
+    async fn on_sync_response(&mut self, network: &mut Network, server: ServerId) {
+        unreachable!("test code only")
+    }
+    async fn on_netsplit(&mut self, network: &mut Network, peer: ServerId, from: ServerId) {
+        unimplemented!()
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    // let engine: Engine<MyHandler> = Engine::new("test".to_string(), |_| MyHandler {});
-    // // let mut handle = engine.handle();
-    // let res = tokio::spawn(async move {
-    //     println!("Running engine");
-    //     // engine.run().await;
-    // });
-    // // handle.shutdown().await;
-    // res.await.unwrap();
-    // ()
+    let engine: Engine<IrcdEvent, Ircd> = Engine::new("hub.a".to_string(), |_| Ircd {});
+    engine.run().await;
+    ()
 }
